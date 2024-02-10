0d723bfe7058a97325654f1b1b7e23f8c0d6bce4
Yura <aderevs@gmail.com>
2024-02-10 13:46:02 +0200

    working version of task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User()
+            {
+                DateOfRegistration = new DateOnly(2022, 1, 16),
+                Email = "user@ukr.net",
+                Name = "Ben",
+                Password = "11111",
+                Usermane = "userneme"
+            };
+            var jsonString = JsonSerializer.Serialize(user);
+
+            using(var writer  = new StreamWriter("user.json"))
+            {
+                writer.WriteLine(jsonString);
+            }
+            using(var reader = new StreamReader("user.json"))
+            {
+                var result = reader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Task5/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class CustomSafeDateTimeConverter : JsonConverter<DateOnly?>
+    {
+
+        public override DateOnly? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var valueString = reader.GetString();
+            if (valueString is null)
+                return null;
+            if (!DateOnly.TryParse(valueString, out var dateTime))
+            {
+                return null;
+            }
+            return dateTime;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly? value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value?.ToString(
+                     "MM/dd/yyyy", CultureInfo.InvariantCulture));
+        }
+    }
+    public partial class CustomSafeEmailConverter : JsonConverter<string?>
+    {
+        public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var email = reader.GetString();
+            if (email is null)
+                return null;
+            var EmailChecker = CheckEmail();
+            if (EmailChecker.IsMatch(email))
+            {
+                return email;
+            }
+            return null;
+        }
+
+        public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value);
+        }
+        
+        
+        [GeneratedRegex(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")]
+        public partial Regex CheckEmail();
+    }
+    internal class User
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("email")]
+        [JsonConverter(typeof(CustomSafeEmailConverter))]
+        public string Email { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Usermane {  get; set; }
+
+        [JsonPropertyName("date_of_registration")]
+        [JsonConverter(typeof(CustomSafeDateTimeConverter))]
+        public DateOnly? DateOfRegistration { get; set; }
+    
+        
+    }
+}
